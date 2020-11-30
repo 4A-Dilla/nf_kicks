d0155f136f5736bf3033b6b191baf3ef73278aa3
sugar_hill <ayofouad@gmail.com>
2020-11-30 11:26:44 +0000

    Refactored code and checked for internet connectivity

--- lib/pages/user_state_page.dart
+++ lib/pages/user_state_page.dart
@@ -1,8 +1,7 @@
-import 'dart:ui';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:nf_kicks/constants.dart';
+import 'package:nf_kicks/pages/loading_page.dart';
 import 'package:provider/provider.dart';
 
 import '../services/auth/base.dart';
@@ -20,37 +19,12 @@ class UserStatePage extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.active) {
           final User user = snapshot.data;
           if (user == null) {
-            print("my user: $user");
             return LoginAndRegistrationPage();
           }
           return LandingPage();
         }
-        return Scaffold(
-          body: Stack(
-            children: [
-              Container(
-                constraints: BoxConstraints.expand(),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/background.jpeg"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
-                child: Container(
-                  color: Colors.orangeAccent.withOpacity(0.2),
-                ),
-              ),
-              SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: kLoadingLogo,
-                ),
-              ),
-            ],
-          ),
+        return Loading(
+          loadingWidget: kLoadingLogo,
         );
       },
     );
