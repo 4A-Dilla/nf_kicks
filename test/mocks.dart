41bb95627333cdb12882aae6adc723b960db67b0
sugar_hill <ayofouad@gmail.com>
2021-01-26 22:28:17 +0000

    Create Mock class for testing

---
+++ test/mocks.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mockito/mockito.dart';
+import 'package:nf_kicks/services/authentication/authentication_api.dart';
+import 'package:nf_kicks/services/database/database_api.dart';
+
+class MockAuth extends Mock implements AuthenticationApi {}
+
+class MockDb extends Mock implements DatabaseApi {}
+
+class MockUser extends Mock implements User {
+  MockUser();
+  factory MockUser.uid(String uid) {
+    final user = MockUser();
+    when(user.uid).thenReturn(uid);
+    return user;
+  }
+}
