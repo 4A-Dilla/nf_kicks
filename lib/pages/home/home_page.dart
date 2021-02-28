8558fbdb9afb9de8e465c599192cf236c67739ae
sugar_hill <ayofouad@gmail.com>
2021-02-28 14:15:33 +0000

    updated home_page.dart with linting rules

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,24 +1,29 @@
-import 'package:firebase_core/firebase_core.dart';
+// Flutter imports:
 import 'package:flutter/material.dart';
-import 'package:nf_kicks/services/authentication/authentication.dart';
-import 'package:nf_kicks/services/authentication/authentication_api.dart';
+
+// Package imports:
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
+// Project imports:
+import 'package:nf_kicks/services/authentication/authentication.dart';
+import 'package:nf_kicks/services/authentication/authentication_api.dart';
 import '../../widgets/constants.dart';
 import '../loading_page.dart';
 import '../something_went_wrong_page.dart';
 import '../user_state_page.dart';
 
 class Home extends StatelessWidget {
-  static const String id = 'home_screen';
+  static final String id = 'home_screen';
   final bool connectionStatus;
   final bool jailbreakOrRootStatus;
   final Future<FirebaseApp> init;
 
-  Home(
-      {@required this.connectionStatus,
-      @required this.jailbreakOrRootStatus,
-      @required this.init});
+  const Home({
+    @required this.connectionStatus,
+    @required this.jailbreakOrRootStatus,
+    @required this.init,
+  });
 
   @override
   Widget build(BuildContext context) {
